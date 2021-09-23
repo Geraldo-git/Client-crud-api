f5dadb0209fd2329f6795006b76896e50ca2c104
Geraldo-git <laboratorio.inicial@gmail.com>
2021-09-23 11:07:16 -0300

    Client CRUD created

---
+++ backend/src/test/java/com/demo/clientcrud/ClientcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.clientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
